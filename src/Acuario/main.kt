92ba1d1a592de316301fa0cf4653d180977c084d
Maykol-23 <maykol.litari@tecsup.edu.pe>
2025-08-28 13:18:13 -0500

    Funcion nueva

--- src/Acuario/main.kt
+++ src/Acuario/main.kt
@@ -7,6 +7,16 @@ fun construirAcuario() {
     val miTorre = TanqueTorre(diametro = 25, alto = 40)
     miTorre.imprimirTamano()
 }
+fun crearPeces() {
+    val tiburon = Tiburon()
+    val pezPayaso = PezPayaso()
+
+    println("El tiburón es de color: ${tiburon.color}")
+    tiburon.comer()
+    println("El pez payaso es de color: ${pezPayaso.color}")
+    pezPayaso.comer()
+}
+
 fun main(){
     construirAcuario()
 }
